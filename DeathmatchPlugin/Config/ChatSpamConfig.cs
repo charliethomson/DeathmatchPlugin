4ed904c1bf968a2f246b0539d2c00f7b3de499ec
Charlie Thomson <charliecthomson@gmail.com>
2023-11-08 17:59:18 -0500

    Update spam text

--- DeathmatchPlugin/Config/ChatSpamConfig.cs
+++ DeathmatchPlugin/Config/ChatSpamConfig.cs
@@ -7,7 +7,7 @@ public static class ChatSpamConfig
     public static readonly List<string> SpamMessages = new()
     {
         Colored.Lime("Type /guns in chat to get a printout (in console) of all weapon commands!"),
-        $"{Colored.Lime("Join Mercury Gaming!")} {Colored.Blue("discord.gg/mercurygaming")}"
+        $"{Colored.Lime("Join our Discord!")} {Colored.Blue("discord.gg/mercurygaming")}"
     };
 
     public static string GetMessage(int spamIndex)
